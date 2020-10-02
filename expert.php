38e36da1c4d7580970e7f04d7c002a2eb6e45a1c
Dwaynebok <dwaynebok19@gmail.com>
2020-10-02 11:55:14 +0200

    done with excercise 2

--- expert.php
+++ expert.php
@@ -6,17 +6,31 @@ ini_set('display_startup_errors',"1");
 error_reporting(E_ALL);
 
 
+// === Exercise 2 ===
 
 function new_exercise() {
-
 
     $x = 1;
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
 
     echo $block;
-
 }
-
-
 new_exercise();
+
+
+
+
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[1];
+
+
+echo $week[0];
+
+
+
+
 
